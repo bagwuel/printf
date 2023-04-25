4d33b138c9514871b6880e4090e065858f6cd839
bagwuel <mbagwu39@gmail.com>
2023-04-25 14:51:12 +0000

    corr t5

--- print_str.c
+++ print_str.c
@@ -16,11 +16,11 @@ int print_str(char *s)
 		return (i);
 	while (*s)
 	{
-		if (*s <= 32)
+		if (*s < 32 || *s == 127)
 		{
 			if (*s <=15)
-				i += _printf("\\x0%X", *s);
-			if (*s > 15 && *s < 33)
+				i = i + _printf("\\x0%X", *s) + 1;
+			if ((*s > 15 && *s < 32) || *s == 127)
 				i += _printf("\\x%X", *s);
 			s++;
 			continue;
